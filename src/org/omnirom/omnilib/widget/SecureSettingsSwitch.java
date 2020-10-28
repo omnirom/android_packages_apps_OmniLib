73e15191a2b3c462319df6a421f12eae53e6afdb
Vachounet <vachounet@live.fr>
2020-10-28 13:26:40 +0100

    OmniLib: add SecureSettingsSwitch and SystemSettingsSwitch
    Change-Id: I0afb459061da29e4007ad8e76c6e02df192d0de6

---
+++ src/org/omnirom/omnilib/widget/SecureSettingsSwitch.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.omnirom.omnilib.widget;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.widget.Switch;
+
+public class SecureSettingsSwitch extends Switch {
+    public SecureSettingsSwitch(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        super.setChecked(Settings.Secure.getInt(getContext().getContentResolver(),
+                getResources().getResourceEntryName(getId()), 0) != 0);
+    }
+
+    public SecureSettingsSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SecureSettingsSwitch(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        Settings.Secure.putInt(getContext().getContentResolver(),
+                getResources().getResourceEntryName(getId()), checked ? 1 : 0);
+        super.setChecked(checked);
+    }
+}
