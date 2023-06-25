426777c867a1da5c9271cfcc360a9a3154b5db56
Michael W <baddaemon87@gmail.com>
2023-06-25 18:38:00 +0200

    ChargingControl: No need to log a crash when not supported
    Instead of relying on catching the exception later on, check if
    ChargingControl is actually supported before checking if a specific
    mode is
    
    Change-Id: Icaf8d333842c9847bea00bedb4b302b7acd49bb3

--- src/org/omnirom/omnilib/internal/health/ChargingControlController.java
+++ src/org/omnirom/omnilib/internal/health/ChargingControlController.java
@@ -255,7 +255,7 @@ public class ChargingControlController extends OmniRomHealthFeature {
 
     public boolean isChargingModeSupported(int mode) {
         try {
-            return (mChargingControl.getSupportedMode() & mode) != 0;
+            return isSupported() && (mChargingControl.getSupportedMode() & mode) != 0;
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }
