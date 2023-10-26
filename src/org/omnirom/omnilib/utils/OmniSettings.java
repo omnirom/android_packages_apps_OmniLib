12f8067923c489a2298953cd6f35a1aa15629cf4
micky387 <mickaelsaibi@free.fr>
2023-10-27 01:50:38 +0200

    OmniLib: Add QS Layout and Tile to OmniSettings
    Change-Id: I394c452a8c5589782e159b72913386d4c244ded0

--- src/org/omnirom/omnilib/utils/OmniSettings.java
+++ src/org/omnirom/omnilib/utils/OmniSettings.java
@@ -186,6 +186,26 @@ public class OmniSettings extends NameValueTable implements BaseColumns {
     public static final String OMNI_USE_OLD_MOBILETYPE = "use_old_mobiletype";
 
     /**
+     * @hide
+     */
+    public static final String OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE = "qs_layout_columns_landscape";
+
+    /**
+     * @hide
+     */
+    public static final String OMNI_QS_LAYOUT_COLUMNS = "qs_layout_columns";
+
+    /**
+     * @hide
+     */
+    public static final String OMNI_QS_TILE_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
+
+    /**
+     * @hide
+     */
+    public static final String OMNI_QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
+
+    /**
      * SettingsBackupAgent will combine its list with this so we dont need
      * to add new things into SettingsProvider SystemSettings
      * @hide
@@ -216,6 +236,10 @@ public class OmniSettings extends NameValueTable implements BaseColumns {
         OMNI_BATTERY_LIGHT_REALLY_FULL_COLOR,
         OMNI_BATTERY_LIGHT_ONLY_FULLY_CHARGED,
         OMNI_USE_OLD_MOBILETYPE,
+        OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE,
+        OMNI_QS_LAYOUT_COLUMNS,
+        OMNI_QS_TILE_VERTICAL_LAYOUT,
+        OMNI_QS_TILE_LABEL_HIDE,
     };
 
     /**
@@ -256,5 +280,9 @@ public class OmniSettings extends NameValueTable implements BaseColumns {
         OMNI_SETTINGS_VALIDATORS.put(OMNI_FAST_CHARGING_LED_ENABLED, 0);
         OMNI_SETTINGS_VALIDATORS.put(OMNI_BATTERY_LIGHT_ONLY_FULLY_CHARGED, 0);
         OMNI_SETTINGS_VALIDATORS.put(OMNI_USE_OLD_MOBILETYPE, 0);
+        OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, 1);
+        OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_LAYOUT_COLUMNS, 1);
+        OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_TILE_VERTICAL_LAYOUT, 0);
+        OMNI_SETTINGS_VALIDATORS.put(OMNI_QS_TILE_LABEL_HIDE, 0);
     }
 }
